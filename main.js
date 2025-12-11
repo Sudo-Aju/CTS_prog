a64255a63bded2e678148d55597fd5cfee65f5aa
Azmeer Pirani <azmeerpirani3@gmail.com>
2025-12-11 12:34:12 +0530

    added html

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ const path = require('path')
 
 function createWindow() {
     const win = new BrowserWindow({
-        width
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+
+        }
     })
+
+    win.loadFile('Maths.html')
+
 }
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
